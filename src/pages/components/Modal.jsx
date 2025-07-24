a0da03db8dbff03afa892830274b95905a1117a3
neatyush <yushpaudel@gmail.com>
2025-07-24 08:01:29 +0545

    sa

--- src/pages/components/Modal.jsx
+++ src/pages/components/Modal.jsx
@@ -20,8 +20,8 @@ const Modal = ({ isOpen, onClose }) => {
       <div className="bg-white rounded-lg p-6 w-11/12 max-w-md shadow-lg">
         <h2 className="text-xl font-bold mb-4 text-[#004e98]">About Me</h2>
         <p className="text-gray-700 text-sm mb-6">
-        Hello, I'm Ayush Paudel — a service-driven hospitality professional with experience in Food & Beverage and Sales, and a strategic focus on customer retention and long-term guest relationships.
-This platform is my small but meaningful attempt to give back — by sharing simplified, actionable resources to help others learn, grow, and build lasting careers in hospitality.
+        Hello, I'm Ayush Paudel — a service-driven hospitality professional with experience in Food & Beverage and Sales, and a strategic focus on customer retention and long-term guest relationships.<br>
+This platform is my small but meaningful attempt to give back — by sharing simplified, actionable resources to help others learn, grow, and build lasting careers in hospitality.</br>
         </p>
         <div className="flex justify-end">
           <button
